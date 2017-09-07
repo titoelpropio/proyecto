1a4d7d61ce560527ce7d06376d05f4e972007711
titoelpropio <titoelpropio77@hotmail.com>
2017-09-07 14:52:58 -0400

    puede ser si

--- local/storage/framework/views/3770e210aab1107d8098e8594b3ff97508d3bf1b.php
+++
@@ -1,7 +0,0 @@
-<div class="form-group">
-		<?php echo Form::label('nombre','nombre:'); ?>
-
-		<?php echo Form::text('nombre',null,['class'=>'form-control','placeholder'=>'ingrese nombre']); ?>
-
-	</div>
-
